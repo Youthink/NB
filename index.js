1c5f13e7866a3b98f96174368f1578b5ef2ea578
Youthink <hufy3651@foxmail.com>
2019-02-02 13:35:52 +0800

    添加图标

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const { app, BrowserWindow } = require('electron')
 
 require('electron-reload')(__dirname);
 
 const path = require('path')
 const url = require('url')
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 995, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 995,
+    height: 600,
+    icon: path.join(__dirname, './src/image/icon.png')
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
